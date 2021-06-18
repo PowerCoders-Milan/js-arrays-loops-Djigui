ebe93d65dc10d994930408d7860dae23296b91be
Djigui <djigui@1F-01.edu-il.it>
2021-06-18 17:09:22 +0200

    terzo esercizio e incompleto, non sono riuscito ad complettarlo

--- 03-multiples-of-number/multiples_of_number.js
+++ 03-multiples-of-number/multiples_of_number.js
@@ -1,1 +1,9 @@
 // define your function here
+const multiplesOf = (number, limit) => {
+    const multiples = [];
+ for (let i = 1; i <= limit / number; i++ ) {
+ multiples[i] = number * i;
+}
+return multiples;
+}
+console.log(multiplesOf(8,400));
