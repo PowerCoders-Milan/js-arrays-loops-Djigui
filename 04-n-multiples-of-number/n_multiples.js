cbc5067611ca7c8b7bcedb09d0136d6eb85cd095
Djigui <djigui@1F-01.edu-il.it>
2021-06-21 10:48:48 +0200

    fatto quarta e sercizio, però mi manca la funzione de while, dopo che avrò finito ultimi esercizio cercherò di completarlo.

--- 04-n-multiples-of-number/n_multiples.js
+++ 04-n-multiples-of-number/n_multiples.js
@@ -1,6 +1,18 @@
-// function declaration
+// function declaration,
+// Questo esercizio è simile ma diverso dal precedente
+//definire una funzione che restituisce un array di n multipli di anumber
+//all'interno della funzione dichiarare una variabile per memorizzare l'array
+//usa un while ciclo per fermarti quando viene soddisfatta una certa condizione💡usare array.lengthper sapere 
+//quando fermarsi;
+//
 const nMultiplesOf = (number, limit) => {
+let array1 = [];
 
+for (let i=1; i<=limit; i++) { 
+array1.push(number*i); 
+}
+return array1;
 }
 
-console.log(nMultiplesOf(2, 10))
+
+console.log(nMultiplesOf(2, 200));
